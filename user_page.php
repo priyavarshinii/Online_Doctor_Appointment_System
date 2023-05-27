614725eb4e2b87ce4f88024c5ce60e7d05ccf143
kadamandla priyavarshini <90304336+priyavarshinii@users.noreply.github.com>
2023-05-27 11:37:11 +0530

    Main user page for users

---
+++ user_page.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DCS-MAIN</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <!-- header section starts -->
+    <header class="header">
+        <a href="#" class="logo"><i class="fas fa-heartbeat"></i>UrFine</a>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#review">review</a>
+            <a href="./doctor/login.php">Doctor Login</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars">
+
+        </div>
+    </header>
+
+    <!-- header section ends -->
+
+    <!-- home sections starts -->
+    <section class="home" id="home">
+        <div class="image">
+            <img src="./images/home-img.svg" alt="">
+        </div>
+        <div class="content">
+            <h3>stay safe, stay healthy</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam ducimus debitis ipsa aliquam enim repudiandae?</p>
+            <a href="./appoint.php" class="btn"> Book Appointment Now! <span class="fas fa-chevron-right"></span> </a>
+        </div>
+    </section>
+    <!-- home sections ends -->
+
+    <!-- service section starts -->
+
+    <!-- <section class="services" id="services">
+        <h1 class="heading"> our <span>services</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <i class="fas fa-notes-medical"></i>
+                <h3>medical camps</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-ambulance"></i>
+                <h3>24/7 ambulance</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-user-md"></i>
+                <h3>Online consultation</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-tablets"></i>
+                <h3>medicines delivery</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-file-medical-alt"></i>
+                <h3>Emergency bookings</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-hand-holding-medical"></i>
+                <h3>total care</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
+            </div>
+        </div>
+    </section> -->
+
+    <!-- service section ends -->
+
+    <!-- icon section starts -->
+    <section class="icons-container">
+        <div class="icons">
+            <i class="fas fa-laptop-medical"></i>
+            <h3>140+</h3>
+            <p>doctors at work</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-users"></i>
+            <h3>1040+</h3>
+            <p>satisfied patients</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-clinic-medical"></i>
+            <h3>700+</h3>
+            <p>medical camps established</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-pills"></i>
+            <h3>2000+</h3>
+            <p>medicines delivered</p>
+        </div>
+    </section>
+
+    <!-- icon section ends -->
+
+        <!-- about section starts -->
+        <section class="about" id="about">
+        <h1 class="heading"> <span>about</span> us </h1>
+        <div class="row">
+            <div class="image">
+                <img src="./images/home-about.jpg" alt="">
+            </div>
+
+            <div class="content">
+                <h3>we take care of your healthy life</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque eos adipisci omnis inventore dolorum, doloribus dolorem quod corrupti enim cupiditate dignissimos quae ex placeat! Provident beatae cum vel dolores vero.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, fugit ducimus! Numquam alias ipsam dolores veniam quos ipsum veritatis in!</p>
+                <!-- <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a> -->
+            </div>
+        </div>
+    </section>
+
+    <!-- about section ends -->
+
+    <!-- review section starts -->
+    <section class="review" id="review">
+        <div class="amb-box">
+            <h1 class="heading">patients's <span>review</span> </h1>
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <h3>Ms.Reena Mathur</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, itaque saepe quia ipsum, doloremque excepturi accusamus in veniam cumque voluptate temporibus velit, minus nesciunt quaerat assumenda praesentium! Praesentium, fugit corporis!
+                </p>
+            </div>
+            <div class="box">
+                <h3>Mr.Ganesh Aggarwal</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni repudiandae vero fugit incidunt nisi sunt, iure dicta obcaecati dolorum facilis adipisci mollitia doloribus! Dolores esse maxime accusantium sunt alias veniam!
+                    </p>
+            </div>
+            <div class="box">
+                <h3>Mr.Rakesh Sharma</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <p class="text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex error consectetur nihil dolore itaque, praesentium esse laboriosam nulla ipsam explicabo quis quos magnam nisi numquam aperiam quidem reprehenderit optio sit.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- review section ends -->
+
+    <!-- footer section starts -->
+    <section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#about"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="./appoint.php"> <i class="fas fa-chevron-right"></i> appointment </a>
+            <a href="./check-appoint.php"> <i class="fas fa-chevron-right"></i> Check Appointment </a>
+        </div>
+        <!-- <div class="box">
+            <h3>services</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> Home checkup </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> medicine delivery </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> 24/7 ambulance </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> medical camps cancer </a>
+        </div> -->
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="tel: +91- 97573 91070" target=""> <i class="fas fa-phone"></i> +91-9757391070 </a>
+            <a href="#"> <i class="fa-brands fa-whatsapp"></i> +91-9757391070 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> info@group10.com </a>
+            <a href="#"> <i class="fas fa-envelope"></i> group10@yahoo.co.in </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> patna, bihar - 800005 </a><!--set location page here too-->
+        </div>
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#" target="blank"><i class="fa-brands fa-facebook"></i>facebook</a>
+            <a href="#" target="blank"><i class="fa-brands fa-instagram"></i>instagram</a>
+            <a href="#" target="blank"><i class="fa-brands fa-linkedin"></i>linkedin</a>
+            <a href="#" target="blank"><i class="fa-brands fa-twitter"></i>twitter</a>
+        </div>
+    </div>
+    <div class="credit"> created by <span> group-10</span></div>
+</section>
+
+    <!-- footer section ends -->
+
+    <!-- custom js file link-->
+    <script src="./js/script.js"></script>
+</body>
+</html>
